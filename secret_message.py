1a6a2e3c50f40e71595b2b0863d89c54de068ca5
Christopher Tate <computate@computate.org>
2022-02-18 20:07:42 -0700

    Added decryption update  for secret message program.

--- secret_message.py
+++ secret_message.py
@@ -4,17 +4,29 @@ Created on Feb 16, 2022
 @author: ctate
 '''
 
+def is_negative_number_digit(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+
 if __name__ == '__main__':
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?/\\[]{};':\"-=_+`~!@#$%^&*()\|ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?/\\[]{};':\"-=_+`~!@#$%^&*()\|"
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?/\\[]{};':\"-=_+`~!@#$%^&*()\| "
+    alphabet = alphabet + alphabet
     stringToEncrypt = input("Please enter a message to encrypt: ")
     stringToEncrypt = stringToEncrypt.upper()
     shiftAmount = input("Please enter a whole number from 1-25 to be your key. ")
-    while not shiftAmount.isnumeric() or int(shiftAmount) < 0 or int(shiftAmount) > 25:
-        shiftAmount = input("Please try again and enter a whole number from 1-25 to be your key. ")
+    while not is_negative_number_digit(shiftAmount) or int(shiftAmount) < -25 or int(shiftAmount) > 25:
+        shiftAmount = input("SOMETHING WENT WRONG. Please try again and enter a whole number from 1-25 to be your key. ")
     shiftAmount = int(shiftAmount)
     encryptedString = ""
     for currentCharacter in stringToEncrypt:
-        position = alphabet.find(currentCharacter)
-        newPosition = position + shiftAmount
+        if shiftAmount < 0:
+            position = alphabet.find(currentCharacter, int(len(alphabet) / 2))
+            newPosition = position + shiftAmount
+        else:
+            position = alphabet.find(currentCharacter)
+            newPosition = position + shiftAmount
         encryptedString = encryptedString + alphabet[newPosition]
     print("Your encrypted message is: ", encryptedString)
